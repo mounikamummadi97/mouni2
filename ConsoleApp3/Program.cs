16ea7585b52314c34d7dd767825eaf5ad09e8d78
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-07 15:24:24 +0530

    sample console

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MyApplication
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("enter username");
+            string userName = Console.ReadLine();
+        Console.WriteLine("username is: " + userName);
+            Console.WriteLine(
+            }
+    }
+    //cp
+
+}
+
